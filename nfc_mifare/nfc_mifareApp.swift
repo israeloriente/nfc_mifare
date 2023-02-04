0dcc2640066177c85b5bcd396f02ec26b2807e59
Israel Oriente <israelnunesoriente@gmail.com>
2023-02-04 11:55:58 +0000

    Initial Commit

---
+++ nfc_mifare/nfc_mifareApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_mifareApp.swift
+//  nfc_mifare
+//
+//  Created by Israel Oriente on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct nfc_mifareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
